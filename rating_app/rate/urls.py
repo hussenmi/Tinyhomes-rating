b4a6a9a3c03adb8f678808f70ce2d1cb7af1c7dd
Hussen <hussenmohammedak@gmail.com>
2021-06-14 03:00:48 -0400

    Initial commit

---
+++ rating_app/rate/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import HomeView, AboutView, CategoryListView, CriteriaListView, SubcriteriaListView
+from . import views
+
+urlpatterns = [
+    path('', HomeView.as_view(), name='rate-home'),
+    path('about/', AboutView.as_view(), name='rate-about'),
+    path('categories/', CategoryListView.as_view(), name='rate-categories'),
+    path('criteria/<int:pk>/', CriteriaListView.as_view(), name='rate-criteria'),
+    path('subcriteria/', SubcriteriaListView.as_view(), name='rate-criteria'),
+]
